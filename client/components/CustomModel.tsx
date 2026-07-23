f85bd1d8b0177be641a14577d74a5c862b3b6df8
MuhammadSaim32 <muhammad.saim.saleem.3.8@gmail.com>
2026-07-23 15:56:52 +0500

    fix: improve error handling in activation response and enhance verification input handling

--- client/components/CustomModel.tsx
+++ client/components/CustomModel.tsx
@@ -1,5 +1,5 @@
 import { Modal, Box } from "@mui/material"
-
+import ClearIcon from '@mui/icons-material/Clear';
 interface Props {
     open: boolean
     setOpen: (open: boolean) => void
@@ -12,25 +12,35 @@ const style = {
     position: 'absolute',
     top: '50%',
     left: '50%',
-    transform: 'translate(-50%, -50%)',
+    transform: 'translate(-50%,-50%)',
     width: 400,
     bgcolor: '#0f172a',
     color: '#f1f5f9',
     border: '2px solid #000',
-    boxShadow: 24,
     p: 4,
 };
 
 const CustomModel = ({ open, setOpen, Component, setRoute }: Props) => {
 
+    const handleClose = (event, reason) => {
+        if (reason == "backdropClick" || reason === 'escapeKeyDown'){
+            return 
+        }
+        setOpen(false)
+        
+    }
+
     return (
         <Modal
             open={open}
-            onClose={() => setOpen(false)}
+            onClose={handleClose}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
         >
             <Box sx={style}>
+                <button className="absolute  cursor-pointer top-2 right-2 bg-red-700 px-1"
+                onClick={handleClose}
+                ><ClearIcon/></button>
                 <Component setRoute={setRoute} setOpen={setOpen} />
             </Box>
         </Modal >
